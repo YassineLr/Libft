d6319898c5997046dfa942d0f62228751500075b
Yassine Larhrissi <Yassine.Lr.2001@gmail.com>
2022-09-07 21:34:05 +0100

    Modifying bonus functions

--- Bonus/ft_lstnew.c
+++ Bonus/ft_lstnew.c
@@ -5,6 +5,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*t__t;
 
 	t__t = (t_list*)malloc(sizeof(size_t));
+	if(!t__t)
+		return (NULL);
 	t__t -> content = content;
 	t__t -> next = NULL;
 	return(t__t);
@@ -15,5 +17,5 @@ int	main()
 	char str[] = "yassine";
 	t_list	*t__t;
 	t__t = ft_lstnew(str);
-	printf("%s", t__t->next);
+	printf("%s", t__t->content);
 }
